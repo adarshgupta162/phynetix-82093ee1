63bdee8919f6a7020ee6b1bbfe7aabcb197d4107
Adarsh Gupta <adarsh07gupta@gmail.com>
2025-12-16 21:08:24 +0530

    Delete supabase directory
    Signed-off-by: Adarsh Gupta <adarsh07gupta@gmail.com>

--- supabase/migrations/20251212044101_52999d74-7fb4-46b5-86d3-4501c140aea9.sql
+++
@@ -1,3 +0,0 @@
--- Add bonus column to test_section_questions
-ALTER TABLE public.test_section_questions
-ADD COLUMN IF NOT EXISTS is_bonus boolean DEFAULT false;
--- supabase/migrations/20251212053437_be964d6d-78ca-4453-8e18-a5fcd73d6446.sql
+++
@@ -1,5 +0,0 @@
--- Add policy for admins to delete test attempts
-CREATE POLICY "Admins can delete test attempts" 
-ON public.test_attempts 
-FOR DELETE 
-USING (has_role(auth.uid(), 'admin'::app_role));
